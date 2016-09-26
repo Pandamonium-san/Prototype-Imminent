479d826a3d90b1311b3fcc574025f8b8492b4c08
Oliver <oliver.nyholm@gmail.com>
2016-09-26 14:47:40 +0200

    Added Lantern class

---
+++ Source/It_PrototypeSound/It_PrototypeSound.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "It_PrototypeSound.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, It_PrototypeSound, "It_PrototypeSound" );
